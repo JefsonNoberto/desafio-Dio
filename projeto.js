7981b90dbf473823d12f915e9605d69392437851
JefsonNoberto <jefsonnoberto0091@gmail.com>
2024-01-15 12:04:30 -0300

    desafio concluido

--- projeto.js
+++ projeto.js
@@ -1,10 +1,22 @@
-let heroi = "jefson";
-let xp = 1000;
-const mensagem = `O Herói de nome ${nome} está no nível de ${nivel}`;
+let heroi = "sonic";
+let xp = 10000;
 let nivel;
 if (xp <= 1000) {
-  nivel = ferro;
-} else if (xp >= 1001 || xp <= 2000) {
-  nivel = Bronze;
+  nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+  nivel = "Prata";
+} else if (xp >= 5001 && xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+  nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
 }
+let mensagem = `O Herói de nome ${heroi} está no nível de ${nivel}`;
 console.log(mensagem);
