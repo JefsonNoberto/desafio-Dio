1f90c936604ef511b43981d448c80694629b163a
JefsonNoberto <jefsonnoberto0091@gmail.com>
2024-01-15 12:19:55 -0300

    first commit

--- projeto.js
+++ projeto.js
@@ -1,1 +1,10 @@
-let poder = "guardião";
+let heroi = "jefson";
+let xp = 1000;
+const mensagem = `O Herói de nome ${nome} está no nível de ${nivel}`;
+let nivel;
+if (xp <= 1000) {
+  nivel = ferro;
+} else if (xp >= 1001 || xp <= 2000) {
+  nivel = Bronze;
+}
+console.log(mensagem);
