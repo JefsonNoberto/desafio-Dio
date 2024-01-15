0c1d96f4a92cfc39dba981ea3703605de60c686a
JefsonNoberto <154082949+JefsonNoberto@users.noreply.github.com>
2024-01-15 16:35:05 -0300

    Atualizar o projeto.js

--- projeto.js
+++ projeto.js
@@ -1,1 +1,7 @@
-let poder = "guardião";
+let herói="jefson"
+let nível = 3000
+if (nível <= 1000){
+console.log(`o nome do herói é ${herói} e o nível dele é prata`)}
+else if (nível >= 1001 || nível<=2000){
+console.log(`o nome do herói é ${herói} e o nível dele é bronze`
+}
